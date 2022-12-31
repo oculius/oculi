a113e1b38b35daadb5bce383a5dc336d7ce83ae5
ravielz <ravielz20@gmail.com>
2023-01-01 04:04:19 +0700

    Implement Oculi Engine

--- rest-server/oculi/engine.go
+++ rest-server/oculi/engine.go
@@ -21,6 +21,7 @@ type (
 		DefaultHTTPErrorHandler(err error, c echo.Context)
 		Pre(middleware ...MiddlewareFunc)
 		Use(middleware ...MiddlewareFunc)
+		UseEchoMiddleware(middleware ...echo.MiddlewareFunc)
 		CONNECT(path string, h HandlerFunc, m ...MiddlewareFunc) Route
 		DELETE(path string, h HandlerFunc, m ...MiddlewareFunc) Route
 		GET(path string, h HandlerFunc, m ...MiddlewareFunc) Route
@@ -37,6 +38,7 @@ type (
 		Add(method string, path string, handler HandlerFunc, middleware ...MiddlewareFunc) Route
 		Host(name string, m ...MiddlewareFunc) RouteGroup
 		Group(prefix string, m ...MiddlewareFunc) RouteGroup
+		RouteGroup(prefix string, m ...MiddlewareFunc) RouteGroup
 		URI(handler HandlerFunc, params ...interface{}) string
 		URL(h HandlerFunc, params ...interface{}) string
 		Reverse(name string, params ...interface{}) string
@@ -59,11 +61,21 @@ type (
 	}
 )
 
-func New(e *echo.Echo) Engine {
+func New() Engine {
+	e := echo.New()
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			oculiCtx, ok := c.(Context)
+			if !ok {
+				oculiCtx = NewContext(c)
+			}
+			return next(oculiCtx)
+		}
+	})
 	return Engine{e}
 }
 
-var _ IEngine = Engine{}
+var _ IEngine = &Engine{}
 
 func (e Engine) NewContext(r *http.Request, w http.ResponseWriter) Context {
 	return C(e.Echo.NewContext(r, w))
@@ -77,112 +89,102 @@ func (e Engine) Use(middleware ...MiddlewareFunc) {
 	e.Echo.Use(mote(middleware)...)
 }
 
+func (e Engine) UseEchoMiddleware(middleware ...echo.MiddlewareFunc) {
+	e.Echo.Use(middleware...)
+}
+
+func (e Engine) generalTransform(path string, h HandlerFunc, m []MiddlewareFunc, fn generalFunc) Route {
+	return NewRoute(fn(path, H(h), mote(m)...))
+}
+
 func (e Engine) CONNECT(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.CONNECT)
 }
 
 func (e Engine) DELETE(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.DELETE)
 }
 
 func (e Engine) GET(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.GET)
 }
 
 func (e Engine) HEAD(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.HEAD)
 }
 
 func (e Engine) OPTIONS(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.OPTIONS)
 }
 
 func (e Engine) PATCH(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.PATCH)
 }
 
 func (e Engine) POST(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.POST)
 }
 
 func (e Engine) PUT(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.PUT)
 }
 
 func (e Engine) TRACE(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.TRACE)
 }
 
 func (e Engine) RouteNotFound(path string, h HandlerFunc, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return e.generalTransform(path, h, m, e.Echo.RouteNotFound)
 }
 
-func (e Engine) Any(path string, handler HandlerFunc, middleware ...MiddlewareFunc) []Route {
-	//TODO implement me
-	panic("implement me")
+func (e Engine) Any(path string, h HandlerFunc, m ...MiddlewareFunc) []Route {
+	return NewRoutes(e.Echo.Any(path, H(h), mote(m)...))
 }
 
-func (e Engine) Match(methods []string, path string, handler HandlerFunc, middleware ...MiddlewareFunc) []Route {
-	//TODO implement me
-	panic("implement me")
+func (e Engine) Match(methods []string, path string, h HandlerFunc, m ...MiddlewareFunc) []Route {
+	return NewRoutes(e.Echo.Match(methods, path, H(h), mote(m)...))
 }
 
 func (e Engine) File(path string, file string, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return NewRoute(e.Echo.File(path, file, mote(m)...))
 }
 
-func (e Engine) Add(method string, path string, handler HandlerFunc, middleware ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+func (e Engine) Add(method string, path string, h HandlerFunc, m ...MiddlewareFunc) Route {
+	return NewRoute(e.Echo.Add(method, path, H(h), mote(m)...))
 }
 
 func (e Engine) Host(name string, m ...MiddlewareFunc) RouteGroup {
-	//TODO implement me
-	panic("implement me")
+	return NewRouteGroup(e.Echo.Host(name, mote(m)...))
 }
 
 func (e Engine) Group(prefix string, m ...MiddlewareFunc) RouteGroup {
-	//TODO implement me
-	panic("implement me")
+	return NewRouteGroup(e.Echo.Group(prefix, mote(m)...))
 }
 
-func (e Engine) URI(handler HandlerFunc, params ...interface{}) string {
-	//TODO implement me
-	panic("implement me")
+func (e Engine) RouteGroup(prefix string, m ...MiddlewareFunc) RouteGroup {
+	return NewRouteGroup(e.Echo.Group(prefix, mote(m)...))
+}
+
+func (e Engine) URI(h HandlerFunc, params ...interface{}) string {
+	return e.Echo.URI(H(h), params...)
 }
 
 func (e Engine) URL(h HandlerFunc, params ...interface{}) string {
-	//TODO implement me
-	panic("implement me")
+	return e.Echo.URL(H(h), params...)
 }
 
 func (e Engine) Routes() []Route {
-	//TODO implement me
-	panic("implement me")
+	return NewRoutes(e.Echo.Routes())
 }
 
 func (e Engine) Static(pathPrefix string, fsRoot string) Route {
-	//TODO implement me
-	panic("implement me")
+	return NewRoute(e.Echo.Static(pathPrefix, fsRoot))
 }
 
 func (e Engine) StaticFS(pathPrefix string, filesystem fs.FS) Route {
-	//TODO implement me
-	panic("implement me")
+	return NewRoute(e.Echo.StaticFS(pathPrefix, filesystem))
 }
 
 func (e Engine) FileFS(path string, file string, filesystem fs.FS, m ...MiddlewareFunc) Route {
-	//TODO implement me
-	panic("implement me")
+	return NewRoute(e.Echo.FileFS(path, file, filesystem, mote(m)...))
 }
