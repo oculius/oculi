8af941bf7d6ccba5f46e660eab6f3d9ddee6f8f0
ravielz <ravielz20@gmail.com>
2023-01-06 02:26:35 +0700

    Polish Dependency Injection

---
+++ common/dependency-injection/boilerplate/rest_server.go
@@ -0,0 +1,90 @@
+package bp_di
+
+import (
+	"context"
+	di "github.com/oculius/oculi/v2/common/dependency-injection"
+	"github.com/oculius/oculi/v2/rest-server"
+	"github.com/oculius/oculi/v2/rest-server/boilerplate"
+	"go.uber.org/fx"
+	"golang.org/x/sys/unix"
+	"sync"
+)
+
+type (
+	lc struct {
+		wg  *sync.WaitGroup
+		srv rest.Server
+	}
+
+	holder struct {
+		opts []fx.Option
+	}
+)
+
+func (h *holder) Dependencies() []fx.Option {
+	return h.opts
+}
+
+func newLCHook(wg *sync.WaitGroup, srv rest.Server) fx.Hook {
+	x := &lc{wg, srv}
+	return fx.Hook{
+		OnStart: x.OnStart,
+		OnStop:  x.OnStop,
+	}
+}
+
+func (l *lc) OnStart(_ context.Context) error {
+	go func() {
+		err := l.srv.Run()
+		if err != nil {
+			panic(err.Error())
+		}
+		l.wg.Done()
+	}()
+	return nil
+}
+
+func (l *lc) OnStop(_ context.Context) error {
+	l.srv.Signal(unix.SIGTERM)
+	return nil
+}
+
+func newRestServer(mc rest.MainController, c rest.Config, res rest.Resource, lc fx.Lifecycle, wg *sync.WaitGroup) (rest.Server, error) {
+	srv, err := rest.New(mc, res, c)
+	if err != nil {
+		return nil, err
+	}
+	lc.Append(newLCHook(wg, srv))
+	return srv, nil
+}
+
+// Register Rest Server Provider & Invoker
+// Required Dependencies: rest.HealthController, rest.Config, rest.Resource, *sync.WaitGroup, rest.Controller
+func RestServer(debug bool) di.ValuableComponent {
+	opts := []fx.Option{
+		di.P(newRestServer),
+		di.I(func(srv rest.Server) {}),
+		di.TP(bp_rest.MainController,
+			nil,
+			[]string{
+				`optional:"false"`,
+				`group:"root_ctrl"`,
+			},
+			nil,
+		),
+		di.TS("v1", []string{`name:"v1"`}),
+		di.TP(bp_rest.RootController,
+			[]string{
+				`group:"root_ctrl"`,
+			},
+			[]string{
+				`name:"v1"`,
+				`group:"v1_ctrl"`,
+			},
+			nil),
+	}
+	if !debug {
+		opts = append(opts, fx.NopLogger)
+	}
+	return &holder{opts}
+}
--- common/dependency-injection/function.go
+++ common/dependency-injection/function.go
@@ -1,68 +1,130 @@
 package di
 
-import "go.uber.org/fx"
+import (
+	"github.com/oculius/optio/iterator"
+	"go.uber.org/fx"
+)
 
 type (
 	function struct {
-		fn        any
-		resultTag string
-		paramTag  string
+		item        any
+		resultTag   []string
+		paramTag    []string
+		asInterface []any
 	}
 )
 
 // P stands for Provider
 func P(fn any) fx.Option {
-	f := &function{fn, "", ""}
-	return f.Build()
+	f := &function{fn, nil, nil, nil}
+	return f.validate().Build()
 }
 
 // I stands for Invoker
 func I(fn any) fx.Option {
-	f := &function{fn, "", ""}
-	return f.Invoke()
+	f := &function{fn, nil, nil, nil}
+	return f.validate().Invoke()
 }
 
 // TP stands for Tagged Provider
-func TP(fn any, resultTag string, paramTag string) fx.Option {
-	f := &function{fn, resultTag, paramTag}
-	return f.Build()
+func TP(fn any, resultTag []string, paramTag []string, asInterface ...interface{}) fx.Option {
+	f := &function{fn, resultTag, paramTag, asInterface}
+	return f.validate().Build()
 }
 
 // D stands for Decorator
 func D(fn any) fx.Option {
-	f := &function{fn, "", ""}
-	return f.Decorate()
+	f := &function{fn, nil, nil, nil}
+	return f.validate().Decorate()
+}
+
+// S stands for Supplier
+func S(item any) fx.Option {
+	f := &function{item, nil, nil, nil}
+	return f.validate().Supply()
+}
+
+// TS stands for Tagged Supplier
+func TS(item any, resultTag []string, asInterface ...interface{}) fx.Option {
+	f := &function{item, resultTag, nil, asInterface}
+	return f.validate().Supply()
 }
 
 func (f *function) Build() fx.Option {
-	var annotations []fx.Annotation
-	if len(f.resultTag) > 0 {
-		annotations = append(annotations, fx.ResultTags(f.resultTag))
-	}
-	if len(f.paramTag) > 0 {
-		annotations = append(annotations, fx.ParamTags(f.paramTag))
-	}
-
-	if len(annotations) == 0 {
-		return fx.Provide(f.fn)
+	ann := f.getAnnotations()
+	if len(ann) == 0 {
+		return fx.Provide(f.item)
 	}
 
 	return fx.Provide(
 		fx.Annotate(
-			f.fn,
-			annotations...,
+			f.item,
+			ann...,
 		),
 	)
 }
 
 func (f *function) Decorate() fx.Option {
 	return fx.Decorate(
-		f.fn,
+		f.item,
 	)
 }
 
 func (f *function) Invoke() fx.Option {
 	return fx.Invoke(
-		f.fn,
+		f.item,
+	)
+}
+
+func (f *function) getAnnotations() []fx.Annotation {
+	var annotations []fx.Annotation
+	if len(f.resultTag) > 0 {
+		annotations = append(annotations, fx.ResultTags(f.resultTag...))
+	}
+	if len(f.paramTag) > 0 {
+		annotations = append(annotations, fx.ParamTags(f.paramTag...))
+	}
+	if f.asInterface != nil {
+		annotations = append(annotations, fx.As(f.asInterface...))
+	}
+	return annotations
+}
+
+func removeEmptyString(input []string) []string {
+	it := iterator.NewFilterIterFromArr[string](input, func(each string) bool {
+		return len(each) > 0
+	})
+	return it.Collect()
+}
+
+func (f *function) validate() *function {
+	if f.item == nil {
+		panic("illegal nil item/function detected")
+	}
+	if len(f.resultTag) > 0 {
+		f.resultTag = removeEmptyString(f.resultTag)
+	}
+	if len(f.paramTag) > 0 {
+		f.paramTag = removeEmptyString(f.paramTag)
+	}
+	if len(f.asInterface) > 0 {
+		it := iterator.NewFilterIterFromArr[any](f.asInterface, func(each any) bool {
+			return each != nil
+		})
+		f.asInterface = it.Collect()
+	}
+	return f
+}
+
+func (f *function) Supply() fx.Option {
+	ann := f.getAnnotations()
+	if len(ann) == 0 {
+		return fx.Supply(f.item)
+	}
+
+	return fx.Supply(
+		fx.Annotate(
+			f.item, ann...,
+		),
 	)
 }
