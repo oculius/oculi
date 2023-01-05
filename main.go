7b9d6998b244bc66ca630c7ce3746f55dd45fd92
ravielz <ravielz20@gmail.com>
2023-01-06 02:32:12 +0700

    example for testing

--- main.go
+++ main.go
@@ -1,25 +1,88 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/oculius/oculi/v2/common/error"
+	di "github.com/oculius/oculi/v2/common/dependency-injection"
+	bp_di "github.com/oculius/oculi/v2/common/dependency-injection/boilerplate"
+	"github.com/oculius/oculi/v2/common/logs"
+	"github.com/oculius/oculi/v2/common/logs/zap"
 	"github.com/oculius/oculi/v2/common/response"
-	"github.com/pkg/errors"
-	"net/http"
+	"github.com/oculius/oculi/v2/rest-server"
+	bp_rest "github.com/oculius/oculi/v2/rest-server/boilerplate"
+	"github.com/oculius/oculi/v2/rest-server/oculi"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+	"sync"
+	"time"
 )
 
+type (
+	Resource struct {
+		rest.Resource
+		fx.In
+	}
+
+	Config struct{}
+
+	HealthController struct{}
+
+	C1 struct{}
+	C2 struct{}
+)
+
+func (c C2) Init(route oculi.RouteGroup) error {
+	apigroup := route.RouteGroup("/c2")
+	apigroup.GET("/asd", func(ctx oculi.Context) error {
+		resp := response.NewOkResponse("hai", Resource{}, true)
+		return ctx.SendPretty(resp)
+	})
+	return nil
+}
+
+func (c C1) Init(route oculi.RouteGroup) error {
+	apigroup := route.RouteGroup("/c1")
+	apigroup.GET("/asd", func(ctx oculi.Context) error {
+		resp := response.NewOkResponse("hola", nil, nil)
+		return ctx.Send(resp)
+	})
+	return nil
+}
+
+var (
+	_ rest.HealthController = HealthController{}
+	_ rest.Controller       = C1{}
+	_ rest.Controller       = C2{}
+)
+
+func (c Config) ServerGracefullyDuration() time.Duration {
+	return time.Second * 5
+}
+
+func (c HealthController) Health() oculi.HandlerFunc {
+	return nil
+}
+func NewResource(l logs.Logger) rest.Resource {
+	return bp_rest.Resource("123", 5555, l)
+}
+
 func main() {
-	const (
-		green  = "\033[97;42m"
-		yellow = "\033[97;33m"
-		reset  = "\033[0m"
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	di.Register(
+		bp_di.RestServer(true),
+		di.S(&wg),
+		di.TS(Config{}, nil, nil, new(rest.Config)),
+		di.TS(zap.AddCallerSkip(1), []string{`group:"log_opts"`}, new(zap.Option)),
+		di.TP(ozap.NewProduction, nil, []string{`group:"log_opts"`}, nil),
+		di.TS(C1{}, []string{`group:"v1_ctrl"`}, new(rest.Controller)),
+		di.TS(C2{}, []string{`group:"v1_ctrl"`}, new(rest.Controller)),
+		di.TS(HealthController{}, nil, new(rest.HealthController)),
+		di.P(NewResource),
+		fx.StartTimeout(time.Second*3),
 	)
-	fmt.Printf("%s %s %s %s %s\n", green, "haiii", reset, yellow, "hello")
-	y := gerr.NewError(errors.New("testing"), "test", http.StatusBadRequest, 456)
-	y2 := response.NewOkResponse("hai", "123", 123)
-	x, _ := json.Marshal(response.New(y))
-	fmt.Println(string(x))
-	x, _ = json.Marshal(response.New(y2))
-	fmt.Println(string(x))
+	deps := di.Dependencies()
+	app := fx.New(deps...)
+
+	app.Run()
+	wg.Wait()
 }
